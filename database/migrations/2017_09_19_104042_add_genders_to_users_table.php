b2753254cb414138b6f085f0233285a84adfaf93
srk_appoets <srk@appoets.com>
2017-09-19 17:43:53 +0530

    Gender Migration Issue Fixed

--- database/migrations/2017_09_19_104042_add_genders_to_users_table.php
+++ database/migrations/2017_09_19_104042_add_genders_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddGenderToUsersTable extends Migration
+class AddGendersToUsersTable extends Migration
 {
     /**
      * Run the migrations.
